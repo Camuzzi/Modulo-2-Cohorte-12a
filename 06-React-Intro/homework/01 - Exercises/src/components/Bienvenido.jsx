f731a906b51079555f0a9e18b2b00ce32b11e416
Camuzzi <camuzziagustin@gmail.com>
2023-03-20 18:12:21 -0300

    1st react proyect

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Agustin Camuzzi";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>Hola! Este es mi primer proyecto con react.</h1>
+      <h3>{studentName}</h3>
+      {techSkills.map(student =>
+        <ul>
+          <li>{student}</li>
+        </ul>
+        )}
+      <Botones alerts={alerts}/>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
